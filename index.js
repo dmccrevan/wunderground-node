8f4029f718c1bb150d2a35f2900127b140409b56
Daniel <dgmccrev@us.ibm.com>
2016-06-09 08:51:42 -0400

    Added new API features & fixed testing

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ var wunderground = function(key, city, state) {
 	this.key = key || null;
 	this.city = city || null;
 	this.state = state || null;
-	this.path = function(type){
-		return '/api/' + key + '/' + type + '/q/' + state + '/' + city + '.json';
+	this.path = function(apiType, date){
+		if(date){
+			if(apiType == 'history_' || apiType == 'planner_'){
+				return '/api/' + key + '/' + apiType + date + '/q/' + state + '/' + city + '.json';
+			}
+			else{
+				console.log("Error: Date field not null without API type of planner or history");
+				return '/api/' + key + '/' + apiType + '/q/' + state + '/' + city + '.json';
+			}
+		}
+		else{
+			if(apiType == 'currenthurricane'){
+				return '/api/' + key + '/' + apiType + '/view.json';
+			}
+			else{
+				return '/api/' + key + '/' + apiType + '/q/' + state + '/' + city + '.json'; 
+			}
+		}
 	};
 	this.conditions = wundergroundApi.conditions;
 	this.forecast = wundergroundApi.forecast;
@@ -13,6 +29,17 @@ var wunderground = function(key, city, state) {
 	this.hourly = wundergroundApi.hourly;
 	this.hourly10day = wundergroundApi.hourly10day;
 	this.alerts = wundergroundApi.alerts;
+	this.almanac = wundergroundApi.almanac;
+	this.astronomy = wundergroundApi.astronomy;
+	this.currentHurricane = wundergroundApi.currentHurricane;
+	this.geolookup = wundergroundApi.geolookup;
+	this.history = wundergroundApi.history;
+	this.planner = wundergroundApi.planner;
+	this.rawtide = wundergroundApi.rawtide;
+	this.satellite = wundergroundApi.satellite;
+	this.tide = wundergroundApi.tide;
+	this.webcams = wundergroundApi.webcams;
+	this.yesterday = wundergroundApi.yesterday;
 };
 
 module.exports = wunderground;
--- test/index-test.js
+++ test/index-test.js
@@ -33,7 +33,7 @@ describe('wunderground api', function(){
 		});
 	});
 	it('.hourly', function(done){
-		client.forecast(function(err, res){
+		client.hourly(function(err, res){
 			if(err) return done(err);
 			res = res.hourly_forecast;
 			res.should.be.an.array;
@@ -41,7 +41,7 @@ describe('wunderground api', function(){
 		});
 	});
 	it('.hourly10day', function(done){
-		client.forecast(function(err, res){
+		client.hourly10day(function(err, res){
 			if(err) return done(err);
 			res = res.hourly_forecast;
 			res.should.be.an.array;
@@ -49,10 +49,98 @@ describe('wunderground api', function(){
 		});
 	});
 	it('.alerts', function(done){
-		client.forecast(function(err, res){
+		client.alerts(function(err, res){
 			if(err) return done(err);
 			res = res.alerts;
 			res.should.be.an.array;
+			return done();
+		});
+	});
+	it('.almanac', function(done){
+		client.almanac(function(err, res){
+			if(err) return done(err);
+			res = res.almanac;
+			res.should.have.property('temp_high');
+			return done();
+		});
+	});
+	it('.astronomy', function(done){
+		client.astronomy(function(err, res){
+			if(err) return done(err);
+			res = res.moon_phase;
+			res.should.have.property('ageOfMoon');
+			return done();
+		});
+	});
+	it('.currentHurricane', function(done){
+		client.currentHurricane(function(err, res){
+			if(err) return done(err);
+			res = res.currenthurricane;
+			res.should.be.an.array;
+			return done();
+		});
+	});
+	it('.geolookup', function(done){
+		client.geolookup(function(err, res){
+			if(err) return done(err);
+			res = res.location;
+			res.should.have.property('country');
+			return done();
+		});
+	});
+	it('.history', function(done){
+		client.history('20110521', function(err, res){
+			if(err) return done(err);
+			res = res.history;
+			res.should.have.property('date');
+			return done();
+		});
+	});
+	it('.planner', function(done){
+		client.planner('05210528', function(err, res){
+			if(err) return done(err);
+			res = res.trip;
+			res.should.have.property('title');
+			return done();
+		});
+	});
+	it('.rawtide', function(done){
+		client.rawtide(function(err, res){
+			if(err) return done(err);
+			res = res.rawtide;
+			res.should.have.property('tideInfo');
+			return done();
+		});
+	});
+	it('.satellite', function(done){
+		client.satellite(function(err, res){
+			if(err) return done(err);
+			res = res.satellite;
+			res.should.have.property('image_url');
+			return done();
+		});
+	});
+	it('.tide', function(done){
+		client.tide(function(err, res){
+			if(err) return done(err);
+			res = res.tide;
+			res.should.have.property('tideInfo');
+			return done();
+		});
+	});
+	it('.webcams', function(done){
+		client.webcams(function(err, res){
+			if(err) return done(err);
+			res = res.webcams;
+			res.should.be.an.array;
+			return done();
+		});
+	});
+	it('.yesterday', function(done){
+		client.yesterday(function(err, res){
+			if(err) return done(err);
+			res = res.history;
+			res.should.have.property('date')
 			return done();
 		});
 	});
